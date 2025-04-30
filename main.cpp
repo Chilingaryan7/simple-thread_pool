2bf0f81cde3bb1a2215f11ad4ce75817e1374aa9
Chilingaryan7 <petros.chilingaryan7@gmail.com>
2025-04-30 21:48:52 +0400

    simple test

--- main.cpp
+++ main.cpp
@@ -1,46 +1,28 @@
 #include "header.hpp"
-void foo1() {
-    std::cout << 1 << std::endl;
-}
-void foo2() {
-    std::cout << 2 << std::endl;
-}
-void foo3() {
-    std::cout << 3 << std::endl;
-}
-void foo4() {
-    std::cout << 4 << std::endl;
-}
-void foo5() {
-    std::cout << 5 << std::endl;
-}
-void foo6() {
-    std::cout << 6 << std::endl;
-}
-void foo7() {
-    std::cout << 7 << std::endl;
-}
-void foo8() {
-    std::cout << 8 << std::endl;
-}
-void foo9() {
-    std::cout << 9 << std::endl;
-}
 
+template <int num> 
+void foo() {
+    std::cout << num << std::endl;
+}
 
 int main(){
     ThreadPool threadpool;
-    threadpool.push_task(foo1);
-    threadpool.push_task(foo2);
-    threadpool.push_task(foo3);
-    threadpool.push_task(foo4);
-    threadpool.push_task(foo5);
-    threadpool.push_task(foo6);
-    threadpool.push_task(foo7);
-    threadpool.push_task(foo8);
-    threadpool.push_task(foo9);
+
+    threadpool.push_task(&foo<1>);
+    threadpool.push_task(&foo<2>);
+    threadpool.push_task(&foo<3>);
+    threadpool.push_task(&foo<4>);
+    threadpool.push_task(&foo<5>);
+    threadpool.push_task(&foo<6>);
+    threadpool.push_task(&foo<7>);
+    threadpool.push_task(&foo<8>);
+    threadpool.push_task(&foo<9>);
+      
     std::this_thread::sleep_for(std::chrono::milliseconds(100)); 
+
     threadpool.end_wark();
+
      std::this_thread::sleep_for(std::chrono::milliseconds(100)); 
+
     return 0;
 }
